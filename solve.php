f2b1891da710e3a98093ffde190d1b9a0d5f8506
Yasser <56389089+yasser-boubani@users.noreply.github.com>
2020-04-23 17:35:55 +0100

    Update solve.php

--- solve.php
+++ solve.php
@@ -19,12 +19,4 @@ if (isset($_GET["equ"]) && $_GET["equ"] != NULL) {
     $json = json_encode($response);
     $json = str_replace("\/", "/", $json);
     echo $json;
-} else { ?>
-    <div>
-        <p>Welcome to the 1st Degree Equation Solver API</p>
-        <a href="?equ=9x%2Bx-15%3D3x%2B9-x">example (9x+x-15=3x+9-x)</a>
-        <p>Note: before sending the GET request you have to encode the special characters of the URI. Click on the example and see the query '?equ=...'</p>
-        <p>The function encodeURIComponent() in JS is used to do that.</p>
-        <a href="javascript:alert('soon! Just after making a documentation for this API :)')">DOWNLOAD the source code with practical demo (github)</a>
-    </div>
-<?php } ?>
+}
